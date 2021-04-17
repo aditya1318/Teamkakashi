b3c72c97843d53002dd5a648de71c02932b4e389
jeet2610 <jeetshah0@icloud.com>
2021-04-17 15:01:27 +0530

    Two Files added

---
+++ app/src/main/java/com/quiz/Model/Cart_Model.kt
@@ -0,0 +1,12 @@
+package com.quiz.Model
+
+data class Cart_model (
+
+ //   val product_id: String? = "123",
+    val product_image: String? ="",
+    val product_name: String? = "Mirage",
+    val product_rate: String? = "10",
+ //   val count : String  = "2",
+
+
+)
