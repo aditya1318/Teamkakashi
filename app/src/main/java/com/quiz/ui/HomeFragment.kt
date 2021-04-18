f48c76af9ecb7b6845e6e3c625ad84df52ac8c82
jeet2610 <jeetshah0@icloud.com>
2021-04-19 00:49:29 +0530

    Added Product to Produdt details page

--- app/src/main/java/com/quiz/ui/HomeFragment.kt
+++ app/src/main/java/com/quiz/ui/HomeFragment.kt
@@ -2,11 +2,14 @@ package com.quiz.ui
 
 import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.os.SharedMemory
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,16 +22,21 @@ import com.quiz.repo.Model.Product_model
 import com.quiz.ProductClickListener
 import com.quiz.ui.adapter.ProductAdapter
 import com.quiz.ecommerce.R
+import com.quiz.viewmodel.Viewmodel
 
 
-class HomeFragment : Fragment(),ProductClickListener {
- private  val db :FirebaseFirestore =  FirebaseFirestore.getInstance();
-    private  val collectionReference :CollectionReference =db.collection("Products");
+class HomeFragment : Fragment(), ProductClickListener {
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance();
+    private val collectionReference: CollectionReference = db.collection("Products");
     private lateinit var adapter: ProductAdapter;
-    lateinit var recyclerView:RecyclerView
+    lateinit var recyclerView: RecyclerView
+    lateinit var vm: Viewmodel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        vm = activity?.let {
+            ViewModelProviders.of(it)[Viewmodel::class.java]
+        } ?: throw Exception("Activity is null")
 
     }
 
@@ -43,53 +51,42 @@ class HomeFragment : Fragment(),ProductClickListener {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
-val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        recyclerView= view.findViewById<RecyclerView>(R.id.Rcview1)
-
-        db.collection("Products")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d(TAG, "${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d(TAG, "Error getting documents: ", exception)
-            }
-
-
-
+        recyclerView = view.findViewById<RecyclerView>(R.id.Rcview1)
 
         return view;
     }
-fun setUpRecyclerView(){
-    val query :Query =FirebaseFirestore.getInstance().collection("Products").orderBy("product_name", Query.Direction.ASCENDING);
-    val options = FirestoreRecyclerOptions.Builder<Product_model>()
 
-        .setQuery(query, Product_model::class.java)
-        .build();
-    adapter = ProductAdapter(options,this)
+    fun setUpRecyclerView() {
+        val query: Query = FirebaseFirestore.getInstance().collection("Products")//.orderBy("product_name", Query.Direction.ASCENDING);
+        val options = FirestoreRecyclerOptions.Builder<Product_model>()
+
+                .setQuery(query, Product_model::class.java)
+                .build();
+        adapter = ProductAdapter(options, this)
 
 //    recyclerView.setAdapter(adapter);
-recyclerView.layoutManager = LinearLayoutManager(activity)
-    recyclerView.adapter = adapter;
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = adapter;
 
 
-
-
-}
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpRecyclerView()
     }
 
+
+
     override fun onProductClickListener(model: Product_model, holder: MaterialCardView) {
+    vm.GetProductModel(model)
+
         holder.findNavController().navigate(R.id.action_homeFragment_to_productDetail)
 
     }
--- app/src/main/java/com/quiz/ui/productDetail.kt
+++ app/src/main/java/com/quiz/ui/productDetail.kt
@@ -9,9 +9,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.navigation.findNavController
+import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.quiz.Model.Cart_model
@@ -19,6 +22,8 @@ import com.quiz.repo.Model.Product_model
 import com.quiz.ecommerce.R
 import com.quiz.repo.repository
 import com.quiz.viewmodel.Viewmodel
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.fragment_product_detail.view.*
 import java.util.List.of
 
 
@@ -37,6 +42,8 @@ class productDetail : Fragment() {
         vm = activity?.let {
             ViewModelProviders.of(it)[Viewmodel::class.java]
         } ?: throw Exception("Activity is null")
+
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -60,6 +67,20 @@ class productDetail : Fragment() {
             vm.addcart(cartModel)
 
         }
+        vm.productitem.observe(viewLifecycleOwner, Observer<Product_model>{
+                view.Product_name.text = it.product_name
+                view.product_des.text = it.product_des
+            Glide.with(view.img1.context)
+                    .load(it.product_image)
+                    .placeholder(R.drawable.ic_microphone_black_48dp)
+                    .into(view.img1)
+            view.product_rate.text = it.product_rate
+            Glide.with(view.imageprofile.context)
+                    .load(it.product_image)
+                    .placeholder(R.drawable.ic_microphone_black_48dp)
+                    .into(view.imageprofile)
+            view.product_rate.text = it.product_rate
+        })
 
 
         return view
