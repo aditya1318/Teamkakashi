97195fbf2ab5b204e218d75ff14430696219c18d
jeet2610 <jeetshah0@icloud.com>
2021-04-18 23:14:31 +0530

    Deleted Files

--- app/src/main/java/com/quiz/ui/ProductClickListner.kt
+++
@@ -1,8 +0,0 @@
-package com.quiz
-
-import com.google.android.material.card.MaterialCardView
-import com.quiz.repo.Model.Product_model
-
-interface ProductClickListener {
-    fun onProductClickListener(model: Product_model, holder: MaterialCardView)
-}
