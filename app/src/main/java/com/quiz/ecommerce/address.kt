f27a6d92a11f82d56724ebae3261950997a1e0d1
jeet2610 <jeetshah0@icloud.com>
2021-04-22 23:48:22 +0530

    Added Payment using Razor

--- app/src/main/java/com/quiz/ecommerce/address.kt
+++ app/src/main/java/com/quiz/ecommerce/address.kt
@@ -1,10 +1,12 @@
 package com.quiz.ecommerce
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,16 +19,22 @@ import com.quiz.ui.adapter.AddressAdapter
 import com.quiz.ui.adapter.Onclickdelete
 import com.quiz.ui.adapter.Onclickedit
 import com.quiz.viewmodel.Viewmodel
+import com.razorpay.Checkout
+import com.razorpay.PaymentData
+import com.razorpay.PaymentResultListener
+import com.razorpay.PaymentResultWithDataListener
 import kotlinx.android.synthetic.main.fragment_address2.view.*
-import kotlinx.android.synthetic.main.fragment_cart.view.*
+import org.json.JSONException
+import org.json.JSONObject
 
 
-class address : Fragment() , Onclickdelete ,Onclickedit{
+class address : Fragment() , Onclickdelete ,Onclickedit,PaymentResultWithDataListener{
 
-
+   // private val TAG: String = address::class.java.getSimpleName()
     private lateinit var adapter: AddressAdapter;
     lateinit var recyclerView: RecyclerView
     lateinit var vm: Viewmodel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +51,32 @@ class address : Fragment() , Onclickdelete ,Onclickedit{
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
+        var amount:String = "1000"
+        var RoundAmount = Math.round(amount.toFloat() * 100)
         val view =inflater.inflate(R.layout.fragment_address2, container, false)
         recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
 
+        view.pay_card.setOnClickListener {
+            Checkout.preload(activity)
+            val checkout = Checkout()
+            checkout.setKeyID("rzp_test_Ae2Nf5hoWLy8gR")
+            checkout.setImage(R.drawable.rzp_logo)
+
+            val options = JSONObject()
+            try {
+                options.put("name", "jams Infotech")
+                options.put("description", "Reference No. #123456")
+                options.put("theme.color", "#3399cc")
+                options.put("currency", "INR")
+                options.put("amount", RoundAmount)
+                options.put("prefill.contact", "8488093696")
+                options.put("prefill.email", "ceo@jams.tech")
+                options.put("send_sms_hash",true)
+                checkout.open(activity, options)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
 
 
         view.add_Address.setOnClickListener {
@@ -64,7 +94,7 @@ class address : Fragment() , Onclickdelete ,Onclickedit{
     }
     private fun openDialog() {
 
-        Address_Dailog.display( fragmentManager)
+        Address_Dailog.display(fragmentManager)
 
     }
 
@@ -73,13 +103,15 @@ class address : Fragment() , Onclickdelete ,Onclickedit{
 
         val userId = vm.getUser_id()
 
-        val query: Query = FirebaseFirestore.getInstance().collection("USER").document(userId!!).collection("address")//.orderBy("product_name", Query.Direction.ASCENDING);
+        val query: Query = FirebaseFirestore.getInstance().collection("USER").document(userId!!).collection(
+            "address"
+        )//.orderBy("product_name", Query.Direction.ASCENDING);
         val options = FirestoreRecyclerOptions.Builder<Address>()
                 .setQuery(query, Address::class.java)
                 .build();
 
 
-        adapter = AddressAdapter(options,this,this)
+        adapter = AddressAdapter(options, this, this)
 
 //    recyclerView.setAdapter(adapter);
 
@@ -107,11 +139,28 @@ class address : Fragment() , Onclickdelete ,Onclickedit{
 
     override fun Onclick2(id: String, address: Address) {
 
-        vm.edit_dailog(id,address,1)
+        vm.edit_dailog(id, address, 1)
         openDialog()
     }
 
+    override fun onPaymentSuccess(p0: String?, p1: PaymentData?) {
+        Toast.makeText(activity,"Payment Success"+p0,Toast.LENGTH_LONG).show()
+    }
 
+    override fun onPaymentError(p0: Int, p1: String?, p2: PaymentData?) {
+        Toast.makeText(activity,"Failed"+p1,Toast.LENGTH_LONG).show()
+    }
+
+    /* override fun onPaymentSuccess(p0: String?) {
+         val builder = AlertDialog.Builder(activity)
+         builder.setTitle("Payment Id")
+         builder.setMessage(p0)
+         builder.show()
+     }
+
+     override fun onPaymentError(p0: Int, p1: String?) {
+         Toast.makeText(activity, p1, Toast.LENGTH_LONG).show()
+     }*/
 
 
 }
---
+++ app/src/main/java/com/quiz/repo/Model/Payment_Model.kt
@@ -0,0 +1,10 @@
+package com.quiz.repo.Model
+
+data class Payment_Model (
+    val user_name: String? = " ",
+    val user_email: String? =" ",
+    val total_price: String? = " ",
+    val user_phonenumber: String? = " ",
+
+        )
+
